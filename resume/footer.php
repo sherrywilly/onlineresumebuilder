fb80775a9e121a7de2360d585a1da6a819939fb8
sherrywilly <sherrywilson521@gmail.com>
2020-10-13 15:12:46 +0530

    removed resume directory

--- resume/footer.php
+++
@@ -1,6 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
